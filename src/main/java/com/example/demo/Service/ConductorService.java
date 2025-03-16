9b7bb774eb2d91ee50827a090d27b3d5949867bd
FaustoMM <faustosanti01@gmail.com>
2025-03-16 12:41:03 -0300

    Service Actualizado (Metodos de ambos Services añadidos)

--- src/main/java/com/example/demo/Service/ConductorService.java
+++ src/main/java/com/example/demo/Service/ConductorService.java
@@ -14,5 +14,43 @@ public class ConductorService {
     @Autowired
     private ConductorRepository conductorRepository;
     
+    //Obtine todos los Conductores verdaderos por estado
+    public List<Conductor> obtenerTodosLosConductores() {
+        return conductorRepository.findByEstadoTrue();
+    }
+
+    
+    //Trae-obtiene un solo Conductor por su id
+    public Conductor obtenerUnConductorPorId (Integer id) {
+        if(id == null) {
+            throw new IllegalArgumentException("El ID NO puede ser nulo.");
+        }
+        return conductorRepository.findById(id).orElse(null);
+    }
+
+
+     //Crea al Conductor
+     public void crearConductor(Conductor conductor) {
+            if (conductor == null) {
+                throw new IllegalArgumentException("El Conductor NO puede ser nulo");
+            }
+            conductorRepository.save(conductor);
+        }
+ 
+
+        //Elimina el cConductor en la vista (Pasa el Estado de True a False en BD)
+        public Conductor EliminarConductor (Integer id) {
+            if (id == null) {
+                throw new IllegalArgumentException("El ID NO PUEDE SER NULO.");
+            }
+            Conductor conductor = conductorRepository.findById(id).orElse(null);
+            if (conductor != null) {
+                conductor.setEstado(false);
+                return conductorRepository.save(conductor);
+            }
+            return null;
+        }
+
+
 }
 
--- src/main/java/com/example/demo/Service/ViajeService.java
+++ src/main/java/com/example/demo/Service/ViajeService.java
@@ -1,10 +1,12 @@
 package com.example.demo.Service;
 
+
 import com.example.demo.Model.Viaje;
 import com.example.demo.Repository.ViajeRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import java.util.List;
+//import java.util.List;
+
 
 @Service
 public class ViajeService {
@@ -12,4 +14,15 @@ public class ViajeService {
     @Autowired
     private ViajeRepository viajeRepository;
 
+    //Guarda un viaje en la BD
+    public Viaje guardarViaje (Viaje viaje) {
+        return viajeRepository.save(viaje);
+    }
+
+    //Busca un Viaje por id
+    public Viaje obtenerUnViajePorId (Long id) {
+        return viajeRepository.findById(id).orElse(null);
+    }
+
+    
 }
